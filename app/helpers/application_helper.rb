bfb86b440d578c3cf00816db986b2b6726611007
esdee303 <steven.deferme@gmail.com>
2025-08-27 20:05:57 +0200

    S05E26 View - Dashboard Main

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
+require "time"
 module ApplicationHelper
-  def logo(size='h2')
+  def logo(size = "h2")
     link_to(root_path, class: "logo #{size}") do
       "<i class=\"bi bi-save-fill me-2\"></i> SafePass".html_safe
     end 
@@ -7,5 +8,14 @@ module ApplicationHelper
 
   def account_page?
     current_page?(edit_user_registration_path)
+  end
+
+  def format_time(time)
+    # Time.strptime(time, "%I%P").strftime("%d/%m/%Y, %H:%M %p")
+    # time.strftime("%d/%m/%Y, %I:%M %p")
+    time.to_s
+    time.ctime
+    time.localtime
+    time.strftime("%d/%m/%Y, %H:%M")
   end
 end
