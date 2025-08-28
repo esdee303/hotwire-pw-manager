ddf50ffa3ec67df2a7edaa2a968c1d18f7fa80c4
esdee303 <steven.deferme@gmail.com>
2025-08-28 02:24:10 +0200

    S05E33 Turbo Frames - Inline Edit UI Pt. 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,59 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Entry.create!(
+  user_id: 2,
+  name: 'Github',
+  url: 'https://github.com',
+  username: 'githubUsername',
+  password: 'Githubpassword123'
+)
+
+Entry.create!(
+  user_id: 2,
+  name: 'Dribble',
+  url: 'https://dribble.com',
+  username: 'dribbleUsername@email.com',
+  password: 'Dribblepassword123'
+)
+
+Entry.create!(
+  user_id: 2,
+  name: 'Stack Overflow',
+  url: 'https://stackoverflow.com',
+  username: 'stackOverflowUsername',
+  password: 'StackOverflowpassword123'
+)
+
+Entry.create!(
+  user_id: 2,
+  name: 'Udemy',
+  url: 'https://udemy.com',
+  username: 'udemyUsername',
+  password: 'Udemypassword123'
+)
+
+Entry.create!(
+  user_id: 2,
+  name: 'Google',
+  url: 'https://google.com',
+  username: 'googleUsername',
+  password: 'googlepassword123'
+)
+
+Entry.create!(
+  user_id: 2,
+  name: 'Amazon',
+  url: 'https://amazon.com',
+  username: 'amazonUsername',
+  password: 'amazonpassword123'
+)
+
+Entry.create!(
+  user_id: 2,
+  name: 'Signal',
+  url: 'https://signal.org',
+  username: 'signalUsername',
+  password: 'signalpassword123'
+)
